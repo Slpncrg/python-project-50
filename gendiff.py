b993e15044be15d4e261e43c373605f21ff7dc74
Slpncrg <536b745a6d62@mail.ru>
2025-05-04 00:09:21 +1100

    stage 4

--- gendiff.py
+++ gendiff.py
@@ -11,3 +11,49 @@ def get_args():
 
 def get_file_data(file_path):
     return json.load(open(file_path))
+
+
+def gendiff(file_path1, file_path2):
+    file1_read = get_file_data(file_path1)
+    file2_read = get_file_data(file_path2)
+    all_keys = sorted(file1_read.keys() | file2_read.keys())
+    
+    diff = {}
+    for key in all_keys:
+        if key not in file1_read.keys():
+            diff[key] = {
+                'type': 'added',
+                'value': file2_read[key]
+            }
+        elif key not in file2_read.keys():
+            diff[key] = {
+                'type': 'removed',
+                'value': file1_read[key]
+            }
+        elif file1_read[key] != file2_read[key]:
+            diff[key] = {
+                'type': 'updated',
+                'value_old': file1_read[key],
+                'value_new': file2_read[key]
+            }
+        else:
+            diff[key] = {
+                'type': 'same',
+                'value': file1_read[key]
+            }
+    return diff
+
+def format(dict):
+    list = []
+    for key, item in dict.items():
+        if item['type'] == 'added':
+            list.append(f'  + {key}: {item['value']}')
+        elif item['type'] == 'removed':
+            list.append(f'  - {key}: {item['value']}')
+        elif item['type'] == 'updated':
+            list.append(f'  - {key}: {item['value_old']}')
+            list.append(f'  + {key}: {item['value_new']}')
+        else:
+            list.append(f'  {key}: {item['value']}')
+
+    return f"{{\n{'\n'.join(list)}\n}}"
--- scripts/gendiff_script.py
+++ scripts/gendiff_script.py
@@ -1,8 +1,10 @@
-from gendiff import get_args, get_file_data
+from gendiff import get_args, get_file_data, gendiff, format
 def main():    
-    args = get_args()
+    #args = get_args()
     file_data = get_file_data('./files/file1.json')
-    
+    diff = gendiff('./files/file1.json', './files/file2.json')
+    format_diff = format(diff)
+    print(format_diff)
 
 
 if __name__ == "__main__":
