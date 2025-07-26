9f96680ebe42dc624a2814717f0318f98c01ee43
Slpncrg <536b745a6d62@mail.ru>
2025-07-27 05:59:09 +1100

    stage 9

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -30,3 +30,16 @@ def test_generate_diff_plain(file_path1, file_path2, expected_result):
     diff = generate_diff(file_path1, file_path2, formatter="plain")
     expected = read_file(expected_result).strip()
     assert diff.strip() == expected
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/fixtures/file1.json',
+     'tests/fixtures/file2.json',
+     'tests/fixtures/expected_json_format.txt'),
+    ('tests/fixtures/file1.yaml',
+     'tests/fixtures/file2.yaml',
+     'tests/fixtures/expected_json_format.txt')])
+def test_generate_diff_json(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter="json")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
